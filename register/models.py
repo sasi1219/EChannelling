46e06de83014fe2c9bf42550ddb9c0c6839c695e
sasini <sasinidr@gmail.com>
2024-06-25 09:43:34 +0530

    Connecting to Database(Register)

--- register/models.py
+++ register/models.py
@@ -1,15 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-from django.db import models
-
-class RegUser(models.Model):
+class New(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=12, unique=True)
-    password = models.CharField(max_length=100)
     nic = models.CharField(max_length=12, unique=True)
     phone = models.CharField(max_length=15)
     dob = models.DateField()
-    gender = models.CharField(max_length=10)
-    terms_agreed = models.BooleanField(default=False)
+    gender = models.CharField(max_length=6, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
+
+    def __str__(self):
+        return self.user.username
+
