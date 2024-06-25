0dda9ab3334a92d9db1e0586840b59b7c6ea95ad
sasini <sasinidr@gmail.com>
2024-06-25 09:58:27 +0530

    Connecting Database to Sign in

--- e_channelling/settings.py
+++ e_channelling/settings.py
@@ -61,10 +61,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'e_channelling.urls'
 
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
