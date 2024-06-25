64b772ff9942b812416aad357bb69f3e31c71bf4
sasini <sasinidr@gmail.com>
2024-06-25 11:14:19 +0530

    Commit Changes

--- e_channelling/settings.py
+++ e_channelling/settings.py
@@ -61,13 +61,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'e_channelling.urls'
 
-import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
