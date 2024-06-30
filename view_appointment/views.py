557b0568175e8d4efa9d7f83ad1cdfc76d0c2614
MeuniB <meuniupeka0917@gmail.com>
2024-06-30 12:47:34 +0530

    View Appointment functionality is activated in the web page.

--- view_appointment/views.py
+++ view_appointment/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 from django.shortcuts import render
+from place_appointment.models import Appointment
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
 def view_appointment(request):
-    return render(request, 'view_appointment.html')
+    # Fetch the logged-in user's appointments
+    user = request.user
+    upcoming_appointments = Appointment.objects.filter(patient__user=user).order_by('appointment_date')
 
+    context = {
+        'upcoming_appointments': upcoming_appointments,
+    }
 
-
-
-
+    return render(request, 'view_appointment.html', context)
